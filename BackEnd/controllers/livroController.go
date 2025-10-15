a098b1e2df2275ee7a1f26b060f9bd40148f2fa9
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2025-10-14 22:12:17 -0300

    teste e atualização

--- BackEnd/controllers/livroController.go
+++ BackEnd/controllers/livroController.go
@@ -120,3 +120,66 @@ func InserirLivro(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, novoLivro)
 }
+
+func AtualizaLivro(c *gin.Context) {
+	collection := config.GetCollection("livro")
+
+	idParam := c.Param("id")
+
+	objID, err := primitive.ObjectIDFromHex(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "ID de livro inválido."})
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var dadosAtualizacao models.Livro
+
+	if err := c.ShouldBindJSON(&dadosAtualizacao); err != nil {
+		// Trata erros de JSON malformado (ex: tipo de dado errado)
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Erro a atualizar livro", "details": err.Error()})
+		return
+	}
+
+	updateDocument := bson.M{
+		"$set": bson.M{
+			"titulo":     dadosAtualizacao.Titulo,
+			"autor":      dadosAtualizacao.Autor,
+			"ano":        dadosAtualizacao.Ano,
+			"genero":     dadosAtualizacao.Genero,
+			"exemplares": dadosAtualizacao.Exemplares,
+			// NOTA: Emprestimos geralmente não é atualizado via PUT/livro,
+			// mas sim em um endpoint separado (ex: POST /livro/:id/emprestimo)
+		},
+	}
+
+	filter := bson.M{"_id": objID}
+
+	result, err := collection.UpdateOne(
+		ctx,
+		filter,
+		updateDocument,
+	)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Falha ao atualizar livro no MongoDB", "details": err.Error()})
+		return
+	}
+
+	if result.ModifiedCount == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Livro não encontrado ou nenhum dado para alterar."})
+		return
+	}
+
+	var livroAtualizado models.Livro
+	err = collection.FindOne(ctx, filter).Decode(&livroAtualizado)
+
+	if err != nil {
+		c.JSON(http.StatusAccepted, gin.H{"message": "Livro atualizado, mas não foi conseguido trazer o documento completo"})
+		return
+	}
+
+	c.JSON(http.StatusOK, livroAtualizado)
+}
--- BackEnd/routes/routes.go
+++ BackEnd/routes/routes.go
@@ -10,4 +10,5 @@ func ConfigurarRotas(router *gin.Engine) {
 	router.GET("/livro", controllers.BuscarLivros)
 	router.GET("/livro/disponiveis", controllers.BuscarLivrosDisponiveis)
 	router.POST("/livro", controllers.InserirLivro)
+	router.PUT("/livro/:id", controllers.AtualizaLivro)
 }
